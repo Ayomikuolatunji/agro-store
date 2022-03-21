d74ec6a47af7728a4645e8a56a12c10a4da77093
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-03-21 21:10:32 +0100

    real estate page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const exportWrapper = document.querySelector(".export")
 const mineral = document.querySelector(".mins")
 const local = document.querySelector(".local")
+const realEstate=document.querySelector(".estate")
 const api="https://roi-investment.herokuapp.com/api/v2/products/"
 
 
@@ -10,19 +11,21 @@ const Err=()=>{
       <h1>Error loading products due network connection</h1>
   </div>`
 }
+
+// fetch export api function call
 function fetchExportProds(){
-    let loader = `<div class="text-center">
-       <h1 class="text-danger ">Fetching products...</h1>
+    let loader = `<div class="d-flex justify-content-center text-center mx-auto">
+    <strong>Loading...</strong>
+    <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
   </div>`;
-    exportWrapper.innerHTML = "";
+    exportWrapper.innerHTML = loader;
    fetch(api)
    .then(data=>{
       return data.json()
    })
    .then(data=>{
     if(data.status==="success"){
-        loader=""
-        exportWrapper.innerHTML = loader;
+        exportWrapper.innerHTML = "";
         const exportProd=data.data.products
         exportProd.forEach(element => {
        if(element.product_type==="EXPORT"){
@@ -58,19 +61,19 @@ function fetchExportProds(){
 }
 
 fetchExportProds()
-// fetch mineral rescources
+// fetch mineral rescources api function call 
 const fetchMineralProds=async()=>{
-    let loader = `<div class="clearfix">
-    <h1 class="text-danger ">Fetching products...</h1>
+    let loader = `<div class="d-flex justify-content-center text-center mx-auto">
+    <strong>Loading...</strong>
+    <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
   </div>`;
-    mineral.innerHTML=""
+    mineral.innerHTML=loader
     fetch(api)
     .then(data=>{
        return data.json()
     })
     .then(data=>{
-        loader ="";
-        mineral.innerHTML=loader
+        mineral.innerHTML=""
         const exportProd=data.data.products
         const fil=exportProd.filter(prod=>{
             if(prod.product_type==="MINERAL"){
@@ -110,9 +113,12 @@ const fetchMineralProds=async()=>{
  
  fetchMineralProds()
 
+
+//  local fetch api function call
  const fetchLocalProds=async()=>{
-    let loader = `<div class="clearfix">
-    <h1 class="text-danger ">Fetching products...</h1>
+    let loader = `<div class="d-flex justify-content-center text-center mx-auto">
+    <strong>Loading...</strong>
+    <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
   </div>`;
     local.innerHTML=loader
     fetch(api)
@@ -157,5 +163,49 @@ const fetchMineralProds=async()=>{
         console.log(err)
     })
  }
- 
  fetchLocalProds()
+
+ //  local fetch api function call
+ const fetchRealEstateProds=async()=>{
+    let loader = `<div class="d-flex justify-content-center text-center mx-auto">
+    <strong>Loading...</strong>
+    <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+  </div>`;
+    realEstate.innerHTML=loader
+    fetch(api)
+    .then(data=>{
+       return data.json()
+    })
+    .then(data=>{
+        realEstate.innerHTML=""
+        const exportProd=data.data.products
+        exportProd.forEach(pro=>{
+            if(pro.product_type==="ESTATE"){
+                realEstate.innerHTML+=`
+            <div class="col-md-6 col-lg-4 col-xl-4">
+                    <div class="blog-box">
+                        <div class="blog-img">
+                            <img class="img-fluid" src=${pro.imgUrl} alt="" />
+                        </div>
+                        <div class="blog-content">
+                            <div class="title-blog">
+                                <h3>Warehouse for sale/let</h3>
+                                <p><span>State:</span> <span>Lagos state </span></p>
+                            </div>
+                            <ul class="option-blog">
+                                <li><a href="#"><i class="far fa-heart"></i></a></li>
+                                <li><a href="#"><i class="fas fa-eye"></i></a></li>
+                                <li><a href="https://api.whatsapp.com/send?phone=+2349122170827&text=I%20want%20to%20enquires%20about%20renting%2Fbuying%20a%20warehouse"><i class="far fa-comments"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+            `
+            }
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+ }
+ fetchRealEstateProds()
